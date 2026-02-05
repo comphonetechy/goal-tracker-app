813255105e1f9655d85b435d4a42ad0044fde28b
Justine John C. Solas <102124186+comphonetechy@users.noreply.github.com>
2026-02-05 15:30:42 +0800

    firebase config

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import firebaseConfigJson from './firebaseconfig.json';
-const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIG || firebaseConfigJson;
+// import firebaseConfigJson from './firebaseconfig.json';
+const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIG // || firebaseConfigJson;
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
