187b2cc3bcbb5e9dc3ffbfd880932d2ddfa99809
Justine John C. Solas <102124186+comphonetechy@users.noreply.github.com>
2026-02-05 15:23:54 +0800

    firebase config git ignore

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBs0CqFuhakRwGCG8zGEgxSiwwF3aO3mB4",
-  authDomain: "goaltrackerapp-371fc.firebaseapp.com",
-  databaseURL: "https://goaltrackerapp-371fc-default-rtdb.firebaseio.com",
-  projectId: "goaltrackerapp-371fc",
-  storageBucket: "goaltrackerapp-371fc.firebasestorage.app",
-  messagingSenderId: "223651168030",
-  appId: "1:223651168030:web:fc05ad7f755b3fe6d9d30f",
-  measurementId: "G-18QJY22BVF"
-};
-
+import firebaseConfigJson from './firebaseconfig.json';
+const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIG || firebaseConfigJson;
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
